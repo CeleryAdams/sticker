325f9cb9e03f692f424183a5552a07d7a79d604d
CeleryAdams <sallyceleryadams@gmail.com>
2024-01-09 16:01:09 -0500

    style load screen

--- src/World.jsx
+++ src/World.jsx
@@ -4,6 +4,8 @@ import Interface from './interface/Interface.jsx'
 import './style.css'
 import Stage from './Stage.jsx'
 import { ToneMapping, EffectComposer, HueSaturation, BrightnessContrast } from '@react-three/postprocessing'
+import { Suspense } from 'react'
+import { Loader, Preload } from "@react-three/drei"
 
 
 export default function World()
@@ -20,14 +22,24 @@ export default function World()
             //     toneMapping: THREE.CineonToneMapping
             // }}
         >
-            <Stage />
-            <Duck />
-            <EffectComposer disableNormalPass>
-                <HueSaturation saturation={0.1}/>
-                <ToneMapping />
-                <BrightnessContrast brightness={0.05} contrast={0.02}/>
-            </EffectComposer>
+            <Suspense fallback={null}>
+                <Stage />
+                <Duck />
+                <EffectComposer disableNormalPass>
+                    <HueSaturation saturation={0.1}/>
+                    <ToneMapping />
+                    <BrightnessContrast brightness={0.05} contrast={0.02}/>
+                </EffectComposer>
+            </Suspense>
         </Canvas>
-        <Interface />   
+        <Suspense fallback={null}>
+            <Interface />   
+        </Suspense>
+        <Loader 
+            containerStyles={{background: '#393B63'}}
+            innerStyles={{width: 300, height: 3, background: '#4F518E'}}
+            barStyles={{height: 3, background: '#787BC1'}}
+            dataStyles={{fontSize: '1em', fontFamily: 'Poppins, "sans-serif"', color: '#787BC1'}}
+        />
     </>
 }
